03ac142ef14bb04dd9a0ded87868294a825f167b
velioo <velioocs@mail.bg>
2016-05-02 18:55:10 +0300

    added italic/bold/underline options

---
+++ Darth Velioo/Java/workspace/VeliooProjects/src/RandomAnimeCharacters.java
@@ -0,0 +1,551 @@
+import java.util.Random;
+
+public class RandomAnimeCharacters {
+
+	public static void main(String[] args) {
+		
+		Random velioo = new Random();
+		int rand;
+		
+		rand = 1 + velioo.nextInt(65);
+		switch(rand){
+		case 1:
+			System.out.print("Mine ");
+			break;
+		case 2:
+			System.out.print("Lelouch ");
+			break;
+		case 3:
+			System.out.print("Light ");
+			break;
+		case 4:
+			System.out.print("Akame ");
+			break;
+		case 5:
+			System.out.print("Hei ");
+			break;
+		case 6:
+			System.out.print("Kiritsugu ");
+			break;
+		case 7:
+			System.out.print("Otonashi ");
+			break;
+		case 8:
+			System.out.print("Kirito ");
+			break;
+		case 9:
+			System.out.print("Asuna ");
+			break;
+		case 10:
+			System.out.print("Yuri ");
+			break;
+		case 11:
+			System.out.print("Kaneki ");
+			break;
+		case 12:
+			System.out.print("Touka ");
+			break;
+		case 13:
+			System.out.print("Saber ");
+			break;
+		case 14:
+			System.out.print("Shinichi ");
+			break;
+		case 15:
+			System.out.print("Black Rock Shooter ");
+			break;
+		case 16:
+			System.out.print("Yuno ");
+			break;
+		case 17:
+			System.out.print("Lubbock ");
+			break;
+		case 18:
+			System.out.print("Sora ");
+			break;
+		case 19:
+			System.out.print("Shiro (NGNL) ");
+			break;
+		case 20:
+			System.out.print("Mato ");
+			break;
+		case 21:
+			System.out.print("Kallen ");
+			break;
+		case 22:
+			System.out.print("L ");
+			break;
+		case 23:
+			System.out.print("Kirigiri ");
+			break;
+		case 24:
+			System.out.print("Levi ");
+			break;
+		case 25:
+			System.out.print("Suzaku ");
+			break;
+		case 26:
+			System.out.print("Asseylum ");
+			break;
+		case 27:
+			System.out.print("Inaho ");
+			break;
+		case 28:
+			System.out.print("Leone ");
+			break;
+		case 29:
+			System.out.print("Yuuki ");
+			break;
+		case 30:
+			System.out.print("Leafa ");
+			break;
+		case 31:
+			System.out.print("Sinon ");
+			break;
+		case 32:
+			System.out.print("Inori ");
+			break;
+		case 33:
+			System.out.print("Suou ");
+			break;
+		case 34:
+			System.out.print("Yin ");
+			break;
+		case 35:
+			System.out.print("Tenshi ");
+			break;
+		case 36:
+			System.out.print("Rin ");
+			break;
+		case 37:
+			System.out.print("Okabe ");
+			break;
+		case 38:
+			System.out.print("Makise ");
+			break;
+		case 39:
+			System.out.print("Shirou ");
+			break;
+		case 40:
+			System.out.print("Shiroe ");
+			break;
+		case 41:
+			System.out.print("Crusty ");
+			break;
+		case 42:
+			System.out.print("Misaki ");
+			break;
+		case 43:
+			System.out.print("Jibril ");
+			break;
+		case 44:
+			System.out.print("Stephanie ");
+			break;
+		case 45:
+			System.out.print("Akatsuki ");
+			break;
+		case 46:
+			System.out.print("Rias ");
+			break;
+		case 47:
+			System.out.print("Xenovia ");
+			break;
+		case 48:
+			System.out.print("Revy ");
+			break;
+		case 49:
+			System.out.print("Rock ");
+			break;
+		case 50:
+			System.out.print("Ryota ");
+			break;
+		case 51:
+			System.out.print("Tatsumi ");
+			break;
+		case 52:
+			System.out.print("Edward ");
+			break;
+		case 53:
+			System.out.print("Alphonse ");
+			break;
+		case 54:
+			System.out.print("Shuu ");
+			break;
+		case 55:
+			System.out.print("Suzuha ");
+			break;
+		case 56:
+			System.out.print("Naegi ");
+			break;
+		case 57:
+			System.out.print("Eren ");
+			break;
+		case 58:
+			System.out.print("Mikasa ");
+			break;
+		case 59:
+			System.out.print("Ayase ");
+			break;
+		case 60:
+			System.out.print("Mao ");
+			break;
+		case 61:
+			System.out.print("Nyanta ");
+			break;
+		case 62:
+			System.out.print("Himiko ");
+			break;
+		case 63:
+			System.out.print("Irisviel ");
+			break;
+		case 64:
+			System.out.print("Senji ");
+			break;
+		case 65:
+			System.out.print("Minatsuki ");
+			break;
+		default:
+			break;
+		}
+		
+		rand = 1 + velioo.nextInt(59);
+		switch(rand){
+		case 1:
+			System.out.print("гледа турски сериали ");
+			break;
+		case 2:
+			System.out.print("си бие главата в клавиатурата ");
+			break;
+		case 3:
+			System.out.print("си хвърля компютъра от 20-я етаж ");
+			break;
+		case 4:
+			System.out.print("играе Minecraft ");
+			break;
+		case 5:
+			System.out.print("косплейва Hei ");
+			break;
+		case 6:
+			System.out.print("краде от техномаркет ");
+			break;
+		case 7:
+			System.out.print("мрази да яде чушки ");
+			break;
+		case 8:
+			System.out.print("се прави на тъп/а ");
+			break;
+		case 9:
+			System.out.print("плува във тоалетната ");
+			break;
+		case 10:
+			System.out.print("краде ток от съседа си ");
+			break;
+		case 11:
+			System.out.print("убива хора само с един ритник ");
+			break;
+		case 12:
+			System.out.print("играе покер в казиното ");
+			break;
+		case 13:
+			System.out.print("кара кола и се гримира ");
+			break;
+		case 14:
+			System.out.print("ходи по улицата на ръце ");
+			break;
+		case 15:
+			System.out.print("лети във въздуха ");
+			break;
+		case 16:
+			System.out.print("повръща от прозореца ");
+			break;
+		case 17:
+			System.out.print("пише домашните си с пръчка ");
+			break;
+		case 18:
+			System.out.print("яде KFC с Калина Динова ");
+			break;
+		case 19:
+			System.out.print("пие вода от тоалетната ");
+			break;
+		case 20:
+			System.out.print("играе League of Legends с един циганин ");
+			break;
+		case 21:
+			System.out.print("бие цигани на улицата всеки ден ");
+			break;
+		case 22:
+			System.out.print("спи в кашон на улицата ");
+			break;
+		case 23:
+			System.out.print("играе федербал с дърво ");
+			break;
+		case 24:
+			System.out.print("се къпе в мивка ");
+			break;
+		case 25:
+			System.out.print("се прави на Мартина Яворска ");
+			break;
+		case 26:
+			System.out.print("се облича като Гандалф ");
+			break;
+		case 27:
+			System.out.print("се прави на Darth Vader ");
+			break;
+		case 28:
+			System.out.print("яде сурово прасе ");
+			break;
+		case 29:
+			System.out.print("си чисти ръцете с циганин ");
+			break;
+		case 30:
+			System.out.print("играе волейбол с Harry Potter ");
+			break;
+		case 31:
+			System.out.print("се бие с Йода ");
+			break;
+		case 32:
+			System.out.print("кара кънки с Бойко Борисов ");
+			break;
+		case 33:
+			System.out.print("бие с кашон Росен Плевнелиев ");
+			break;
+		case 34:
+			System.out.print("стреля с пистолет по Джъстин Бийбър ");
+			break;
+		case 35:
+			System.out.print("рита легнал негър ");
+			break;
+		case 36:
+			System.out.print("гледа Аниме ");
+			break;
+		case 37:
+			System.out.print("яде кренвиршка ");
+			break;
+		case 38:
+			System.out.print("си чисти ушите с клечка за зъби ");
+			break;
+		case 39:
+			System.out.print("яде кон ");
+			break;
+		case 40:
+			System.out.print("спи в салфетка ");
+			break;
+		case 41:
+			System.out.print("тайно планира да те убие ");
+			break;
+		case 42:
+			System.out.print("иска да играе Minecraft с теб ");
+			break;
+		case 43:
+			System.out.print("иска да ти удари 2 шамара ");
+			break;
+		case 44:
+			System.out.print("те гледа в момента ");
+			break;
+		case 45:
+			System.out.print("ти купува подарък от Теранова ");
+			break;
+		case 46:
+			System.out.print("идва да те срита ");
+			break;
+		case 47:
+			System.out.print("се прави на твой домашен любимец ");
+			break;
+		case 48:
+			System.out.print("пие Кока-Кола Light ");
+			break;
+		case 49:
+			System.out.print("рови в кофа за боклук ");
+			break;
+		case 50:
+			System.out.print("хвърля камъни по станал негър ");
+			break;
+		case 51:
+			System.out.print("е създал/а вируса Ебола ");
+			break;
+		case 52:
+			System.out.print("се прави на маймуна ");
+			break;
+		case 53:
+			System.out.print("си реже ноктите ");
+			break;
+		case 54:
+			System.out.print("играе футбол ");
+			break;
+		case 55:
+			System.out.print("си прави маникюр ");
+			break;
+		case 56:
+			System.out.print("яде Дени ");
+			break;
+		case 57:
+			System.out.print("си играе на телефона ");
+			break;
+		case 58:
+			System.out.print("се е прицелил/а със снайпер в теб ");
+			break;
+		case 59:
+			System.out.print("си гледа фейсбука ");
+			break;
+		default:
+			break;
+		}
+		rand = 1 + velioo.nextInt(50);
+		switch(rand){
+		case 1:
+			System.out.print("докато спи ");
+			break;
+		case 2:
+			System.out.print("докато се хвърля от блока ");
+			break;
+		case 3:
+			System.out.print("докато играе футбол ");
+			break;
+		case 4:
+			System.out.print("докато седи на стол ");
+			break;
+		case 5:
+			System.out.print("докато прави задно салто ");
+			break;
+		case 6:
+			System.out.print("докато гледа филм ");
+			break;
+		case 7:
+			System.out.print("докато яде спагети ");
+			break;
+		case 8:
+			System.out.print("докато лети в космоса ");
+			break;
+		case 9:
+			System.out.print("докато чака абтобуса на спирката ");
+			break;
+		case 10:
+			System.out.print("докато се бие с Пери Птицечовката ");
+			break;
+		case 11:
+			System.out.print("докато е в тоалетната ");
+			break;
+		case 12:
+			System.out.print("докато се вози в самолет ");
+			break;
+		case 13:
+			System.out.print("докато се прави на кокошка ");
+			break;
+		case 14:
+			System.out.print("докато се бие с Батман ");
+			break;
+		case 15:
+			System.out.print("докато си мие ръцете на мивка ");
+			break;
+		case 16:
+			System.out.print("докато е на пикник с Куай-Гон Джин ");
+			break;
+		case 17:
+			System.out.print("докато си купува продукти за пица от магазина ");
+			break;
+		case 18:
+			System.out.print("докато бие Дени на улицата ");
+			break;
+		case 19:
+			System.out.print("докато плува в морето ");
+			break;
+		case 20:
+			System.out.print("докато си пие сутрешното кафе ");
+			break;
+		case 21:
+			System.out.print("докато пие кока-кола ");
+			break;
+		case 22:
+			System.out.print("докато прави челна стойка ");
+			break;
+		case 23:
+			System.out.print("докато танцува хоро ");
+			break;
+		case 24:
+			System.out.print("докато си играе с китаец на площадката ");
+			break;
+		case 25:
+			System.out.print("докато чете Библията ");
+			break;
+		case 26:
+			System.out.print("когато няма какво да яде ");
+			break;
+		case 27:
+			System.out.print("докато отива до Мола ");
+			break;
+		case 28:
+			System.out.print("докато чете детски книжки ");
+			break;
+		case 29:
+			System.out.print("докато мие чиниите след вечеря ");
+			break;
+		case 30:
+			System.out.print("докато лети с ракета до Луната ");
+			break;
+		case 31:
+			System.out.print("всеки ден ");
+			break;
+		case 32:
+			System.out.print("докато крещи на улицата ");
+			break;
+		case 33:
+			System.out.print("докато се катери по дърво ");
+			break;
+		case 34:
+			System.out.print("докато яде боб ");
+			break;
+		case 35:
+			System.out.print("докато се крие в кофа за боклук ");
+			break;
+		case 36:
+			System.out.print("докато играе шах в кофа за боклук ");
+			break;
+		case 37:
+			System.out.print("докато копа картофи ");
+			break;
+		case 38:
+			System.out.print("докато си полива цветята ");
+			break;
+		case 39:
+			System.out.print("докато замерва Венелин Йоргов с камъни ");
+			break;
+		case 40:
+			System.out.print("докато свири на китара ");
+			break;
+		case 41:
+			System.out.print("докато се разхожда в парка ");
+			break;
+		case 42:
+			System.out.print("докато се пече на плажа ");
+			break;
+		case 43:
+			System.out.print("докато си приготвя вечеря ");
+			break;
+		case 44:
+			System.out.print("докато закусва ");
+			break;
+		case 45:
+			System.out.print("докато рита летящ циганин ");
+			break;
+		case 46:
+			System.out.print("когато е пиян/а ");
+			break;
+		case 47:
+			System.out.print("докато се прави на китаец/ка ");
+			break;
+		case 48:
+			System.out.print("докато слуша музика ");
+			break;
+		case 49:
+			System.out.print("докато замерва Бойко Борисов с циганин ");
+			break;
+		case 50:
+			System.out.print("докато кара колело ");
+			break;
+		default:
+			break;
+		}
+		
+	}
+
+}
